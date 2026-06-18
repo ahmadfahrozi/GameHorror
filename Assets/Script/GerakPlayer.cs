a0b506ec67343ac29479290acc3c5ad819e14b0d
ahmadfahrozi <ahmadrozi163@gmail.com>
2026-06-18 16:15:22 +0700

    Initial commit

---
+++ Assets/Script/GerakPlayer.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class GerakPlayer : MonoBehaviour
+{
+    public float speed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+        moveInput = moveInput.normalized;
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = moveInput * speed;
+    }
+}
